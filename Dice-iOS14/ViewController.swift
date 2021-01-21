e4412545e80b0a1aad088b1719a3a7b54c488003
dylpark <dylanpark.design@gmail.com>
2021-01-21 10:13:03 +1000

    Initial Commit

---
+++ Dice-iOS14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice-iOS14
+//
+//  Created by Dylan Park on 21/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
